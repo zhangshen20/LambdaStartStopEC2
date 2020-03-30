c10d14b906fda79a362a7b3afab9ad4cfba2a3b0
zhangshen20 <zhangshen20@hotmail.com>
2020-03-31 02:14:23 +1100

    V1.7

--- lambda/stopec2.py
+++ lambda/stopec2.py
@@ -1,8 +1,9 @@
 import boto3
 
-region = 'us-west-1'
+region = 'ap-southeast-2'
 
-instances = ['i-12345cb6de4f78g9h', 'i-08ce9b2d7eccf6d26']
+instances = ['i-03a5bb91ec9e697df']
+
 ec2 = boto3.client('ec2', region_name=region)
 
 def lambda_handler(event, context):
